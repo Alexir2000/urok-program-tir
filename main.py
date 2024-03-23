c0177e51ccd1db0406c23128d2695b577940a457
Alexir2000 <hit2007@list.ru>
2024-03-23 21:30:24 +0300

    добавление всех констант

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -8,7 +9,17 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption(" Игра ТИР")
 
-icon = pygame.image.load()
+icon = pygame.image.load("img/icon.png")
+pygame.display.set_icon(icon)
+
+targer_img = pygame.image.load("img/target_img.png")
+targer_width = 50
+targer_height = 50
+
+target_x = random.randint(0,SCREEN_WIDTH - targer_width)
+target_y = random.randint(0,SCREEN_HEIGHT - targer_height)
+
+color = (random.randint(0,255), random.randint(0,255),random.randint(0,255))
 
 running = true
 
